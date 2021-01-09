832da7f592bd16bac03d46bbd1855829b4c7f4ed
igor <deonarius@gmail.com>
2021-01-10 00:52:37 +0200

    fix

--- prices-wholesalers.php
+++ prices-wholesalers.php
@@ -41,46 +41,7 @@
           <!-- Phone form end -->
         </div>
         <div class="d-none d-lg-block col-12 col-lg-4">
-          <div class="curs-metal">
-            <div class="curs-metal__title">
-              <h3>
-                <b>Честные выплаты до <span class="text-orange">75%</span> по LME</b>
-              </h3>
-              <div class="curs-metal__icon">
-                <img src="assets/img/icons/icon-metal.svg" alt="" />
-              </div>
-            </div>
-            <div class="curs-metal-list">
-              <div class="curs-metal-list__item d-flex justify-content-between text-placeholder">
-                <div class="small-text">Сегодня</div>
-                <div class="small-text">Вчера</div>
-              </div>
-              <div class="curs-metal-list__item">
-                <div class="curs-metal__valuta">Pd<span class="curs-metal__valuta_up"></span></div>
-                <div class="d-flex justify-content-between">
-                  <div class="h4">5 658,74 ₽/г</div>
-                  <div class="h5 text-placeholder">5 648,74 ₽/г</div>
-                </div>
-              </div>
-              <div class="curs-metal-list__item">
-                <div class="curs-metal__valuta">Pd<span class="curs-metal__valuta_up"></span></div>
-                <div class="d-flex justify-content-between">
-                  <div class="h4">5 658,74 ₽/г</div>
-                  <div class="h5 text-placeholder">5 648,74 ₽/г</div>
-                </div>
-              </div>
-              <div class="curs-metal-list__item">
-                <div class="curs-metal__valuta">Pd<span class="curs-metal__valuta_down"></span></div>
-                <div class="d-flex justify-content-between">
-                  <div class="h4">5 658,74 ₽/г</div>
-                  <div class="h5 text-placeholder">5 648,74 ₽/г</div>
-                </div>
-              </div>
-            </div>
-            <div class="curs-metal__link text-center">
-              <a href="https://www.lme.com/" target="_blank" class="link"> проверить на lme.com <img class="ml-10" src="assets/img/icons/icon-arrow-box.svg" alt="#" /></a>
-            </div>
-          </div>
+          <?php include("templates/curs-metal.php"); ?>
         </div>
       </div>
     </div>
